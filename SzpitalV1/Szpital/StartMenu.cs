debde191cf8939b8b47f66171a718075d4da7497
kielbassa <63872557+kielbassa@users.noreply.github.com>
2023-10-21 20:43:51 +0200

    Basic struktura kodu z podzialem na klasy

---
+++ SzpitalV1/Szpital/StartMenu.cs
@@ -0,0 +1,23 @@
+﻿namespace Program
+{
+    public class Menu
+    {
+        int windowHeight = 30;
+        int windowWidth = 100;
+        public void SetWindowSize()  // ustawianie odpowieniego rozmiaru okna konsoli ze sprawdzeniem czy nie jest wiekszy od dostepnego ekranu
+        {
+            
+            int maxWindowHeight = Console.LargestWindowHeight;
+            int maxWindowWidth = Console.LargestWindowWidth;
+            if(maxWindowHeight > windowHeight || maxWindowWidth > windowWidth)
+            {
+                Console.SetWindowSize(windowWidth, windowHeight);
+            }
+            else
+            {
+                Console.WriteLine("Skala konsoli nie może być ustawiona");
+                Environment.Exit(1);
+            }
+        }
+    }
+}
---
+++ SzpitalV1/Szpital/Szpital.cs
@@ -0,0 +1,11 @@
+﻿namespace Program
+{
+    class Szpital
+    {
+        public void Start()
+        {
+            Menu menu = new Menu();
+            menu.SetWindowSize();
+        }
+    }
+}
