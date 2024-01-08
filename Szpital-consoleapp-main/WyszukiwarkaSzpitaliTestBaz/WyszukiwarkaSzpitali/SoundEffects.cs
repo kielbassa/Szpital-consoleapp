0cef55e7453b1423d69fe088043754b412334bbb
Karol Lademan <51699@student.umg.edu.pl>
2024-01-08 10:08:08 +0100

    Dodano Preferences.txt

--- Szpital-consoleapp-main/WyszukiwarkaSzpitaliTestBaz/WyszukiwarkaSzpitali/SoundEffects.cs
+++ Szpital-consoleapp-main/WyszukiwarkaSzpitaliTestBaz/WyszukiwarkaSzpitali/SoundEffects.cs
@@ -1,10 +1,12 @@
-﻿namespace Program
+﻿#pragma warning disable CA1416
+namespace Program
 {
     public class SoundEffects
     {
         public void StartJingle(bool soundToggle)
         {
-            if (soundToggle) {
+            if (soundToggle && System.OperatingSystem.IsWindows())
+            {
                 Console.Beep(300, 100);
                 Console.Beep(600, 100);
                 Console.Beep(1200, 200);
@@ -12,7 +14,8 @@
         }
         public void CycleOption(bool soundToggle)
         {
-            if(soundToggle) {
+            if (soundToggle && System.OperatingSystem.IsWindows())
+            {
                 Console.Beep(400, 50);
                 Console.Beep(800, 80);
             }
@@ -20,7 +23,8 @@
         }
         public void BackOption(bool soundToggle)
         {
-            if(soundToggle) {
+            if (soundToggle && System.OperatingSystem.IsWindows())
+            {
                 Console.Beep(600, 50);
                 Console.Beep(300, 100);
             }
@@ -28,7 +32,8 @@
         }
         public void StopJingle(bool soundToggle)
         {
-            if (soundToggle) {
+            if (soundToggle && System.OperatingSystem.IsWindows())
+            {
                 Console.Beep(1000, 100);
                 Console.Beep(600, 100);
                 Console.Beep(300, 100);
